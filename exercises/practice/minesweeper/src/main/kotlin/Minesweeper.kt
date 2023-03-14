865d60a58d35c299aee3958c730b97e7c59716f5
Katrina Owen <katrina.owen@gmail.com>
2023-03-14 11:01:36 +0100

    Sync sieve docs with problem-specifications (#564)
    The sieve exercise has been overhauled as part of a project
    to make practice exercises more consistent and friendly.
    
    For more context, please see the discussion in the forum, as well as
    the pull request that updated the exercise in the problem-specifications
    repository:
    
    - https://forum.exercism.org/t/new-project-making-practice-exercises-more-consistent-and-human-across-exercism/3943
    - https://github.com/exercism/problem-specifications/pull/2216
    
    ----
    
    If you approve this pull request, I will eventually merge it. However, if you are happy with this change **please merge the pull request**, as it will get the changes into the hands of the students much more quickly.

--- exercises/practice/minesweeper/src/main/kotlin/Minesweeper.kt
+++ exercises/practice/minesweeper/src/main/kotlin/Minesweeper.kt
@@ -1,8 +1,30 @@
-data class MinesweeperBoard(val todo: Nothing) {
+data class MinesweeperBoard(val inputBoard: List<String>) {
 
     // TODO: Implement proper constructor
 
-    fun withNumbers(): List<String> {
-        TODO("Implement this function to complete the task")
+    fun withNumbers(): List<String> =
+        inputBoard.mapIndexed { row, inputStr-> //The inputB is the list of strings
+            inputStr.mapIndexed { col, cell -> when (cell) { //Map applied to a string
+                ' ' -> adjacentMines(row, col)
+                else -> cell
+            }
+            }.joinToString("")
+
+        }
+
+    private fun adjacentMines(row: Int, col: Int): Char {
+        var count = 0;
+        var rangeRow = Math.max(0, row - 1)..Math.min(inputBoard.size - 1, row + 1);
+        var rangeCol = Math.max(0, col - 1)..Math.min(inputBoard[0].length - 1, col + 1);
+        for (i in rangeRow){
+            for (j in rangeCol){
+                if (inputBoard[i][j] =='*'){
+                    count=count+1
+                } else{
+                    count=count
+                }
+            }
+        }
+        return if (count > 0) (count + 48).toChar() else ' '
     }
 }
